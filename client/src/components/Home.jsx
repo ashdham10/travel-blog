b8f070e20ab87e9e68e977adbc835451d0f0c48f
ashdham10 <ashleydenham10@gmail.com>
2018-11-14 11:57:23 -0600

    cards moved

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -23,11 +23,11 @@ class Home extends Component {
         return this.state.blogs.map(blog => {
             return (
                 <div className="row" key={blog.id} id="blogCard">
-                <div className="card" style={{width: '18rem'}}>
+                <div className="card bg-dark" style={{width: '18rem'}}>
                     <img className="card-img-top" src="/road-trip.jpg" alt="img-placeholder" />
                     <div className="card-body">
                         <h5 className="card-title">{blog.title}</h5>
-                        <Link to={`/blogs/${blog.id}`} className="btn btn-secondary">Read More..</Link>
+                        <Link to={`/blogs/${blog.id}`} className="btn btn-secondary btn-sm">Read More..</Link>
                     </div>
                 </div>
                 </div>
@@ -37,15 +37,3 @@ class Home extends Component {
 };
 
 export default Home;
-
-
-
-    // <div className="row" key={blog.id} id="blogCard">
-                //         <div className="card col-6 mt-20">
-                //         <div className="card-header">{blog.title}</div>
-                //             <div className="card-body">
-                //                 <p>{blog.content}</p>
-                //                 <Link to={`/blogs/${blog.id}`} className="btn btn-secondary">Read More..</Link>
-                //             </div>
-                //         </div>
-                //     </div>
